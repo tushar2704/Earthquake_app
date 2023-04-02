556689371f6ab8b9114562d8ef1b5fc435afac60
tushar2704 <tushar.inseec@gmail.com>
2023-04-02 13:22:22 +0530

    app

---
+++ README.md
@@ -0,0 +1,5 @@
+## Earthquake_Notification_App by Tushar Aggarwal, github.com/tushar2704
+
+```
+streamlit run Earthquake_notify_app.py
+```
--- app2.py
+++ app2.py
@@ -1,11 +1,7 @@
-## Earthquake_Notification_App by Tushar Aggarwal, github.com/tushar2704
-
-# Importing required libraries
 import streamlit as st
 import requests
 import time
-import winsound
-from PIL import Image
+import webbrowser
 import folium
 from streamlit_folium import folium_static
 
@@ -22,10 +18,7 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-# Load image and sound file
-image = Image.open("earthquake.png")
-notification_sound = "SystemExit"
-
+# Define function to get latest earthquake data
 def get_latest_earthquake():
     url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson"
     response = requests.get(url).json()
@@ -33,15 +26,23 @@ def get_latest_earthquake():
     latest = features[0]
     return latest
 
+# Define function to display earthquake information and map
 def display_earthquake_info(earthquake):
-    col1, col2, col3 = st.columns([1, 1, 2])
+    # Create columns to display earthquake information and map
+    col1, col2 = st.columns([1, 2])
+    
     with col1:
-        st.image(image, use_column_width=True)
-    with col2:
+        # Display earthquake information
+        st.image("https://www.shareicon.net/data/128x128/2015/08/06/80805_disaster_512x512.png", use_column_width=True)
         st.write(f"**Magnitude:** {earthquake['properties']['mag']}")
         st.write(f"**Location:** {earthquake['properties']['place']}")
         st.write(f"**Time:** {time.ctime(int(earthquake['properties']['time']/1000))}")
-    with col3:
+        
+        # Add a button to open the USGS website with more information
+        if st.button("View More Information"):
+            webbrowser.open_new_tab(earthquake['properties']['url'])
+        
+    with col2:
         # Create a map centered on the earthquake location
         map_center = [earthquake['geometry']['coordinates'][1], earthquake['geometry']['coordinates'][0]]
         m = folium.Map(location=map_center, zoom_start=8, tiles='OpenStreetMap')
@@ -49,29 +50,23 @@ def display_earthquake_info(earthquake):
         # Convert the map to HTML and display it in a Streamlit column
         folium_static(m, width=700)
 
-def play_notification_sound():
-    winsound.PlaySound(notification_sound, winsound.SND_ALIAS)
-
+# Define main function to continuously check for new earthquakes
 def main():
     st.title("Latest Earthquake Notifier")
     st.subheader("Get notified about the latest earthquake!")
     st.markdown("---")
     
+    # Get latest earthquake data and display it
     latest_earthquake = get_latest_earthquake()
     display_earthquake_info(latest_earthquake)
     
-    play_notification = st.checkbox("Enable notification for new earthquake?")
-
-    while play_notification:
-        time.sleep(60) # Check every minute
+    # Check for new earthquakes every minute
+    while True:
+        time.sleep(60)
         new_earthquake = get_latest_earthquake()
         if new_earthquake["id"] != latest_earthquake["id"]:
             latest_earthquake = new_earthquake
             display_earthquake_info(latest_earthquake)
-            play_notification_sound()
 
 if __name__ == "__main__":
     main()
-
-
-###########################################################################################
